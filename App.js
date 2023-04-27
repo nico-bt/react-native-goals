80169ece0db7a5414aa488132472d504b88024d7
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-04-27 20:39:20 -0300

    Basic structure and styles

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { Button, StyleSheet, Text, TextInput, View } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Enter a goal" style={styles.input} />
+        <Button title="Add Goal" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals here...</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24,
   },
-});
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    gap: 6,
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "gray",
+  },
+  input: {
+    flex: 1,
+    borderColor: "gray",
+    borderWidth: 2,
+    borderRadius: 12,
+    padding: 6,
+  },
+  goalsContainer: {
+    flex: 6,
+  },
+})
