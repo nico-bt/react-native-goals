bcc6fe29e33b37eb96abefdc8055155d4337e4e6
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-04-28 23:33:08 -0300

    Adding and showing the goals

--- App.js
+++ App.js
@@ -1,15 +1,54 @@
 import { StatusBar } from "expo-status-bar"
-import { Button, StyleSheet, Text, TextInput, View } from "react-native"
+import { useState } from "react"
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native"
 
 export default function App() {
+  const [goalInput, setGoalInput] = useState("")
+  const [goals, setGoals] = useState([])
+
+  const inputHandler = (text) => {
+    setGoalInput(text)
+  }
+
+  const addGoalHandler = () => {
+    setGoals((prevState) => [
+      ...prevState,
+      { text: goalInput, key: Math.random().toString() },
+    ])
+    setGoalInput("")
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Enter a goal" style={styles.input} />
-        <Button title="Add Goal" />
+        <TextInput
+          placeholder="Enter a goal"
+          style={styles.input}
+          onChangeText={inputHandler}
+          value={goalInput}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
+
       <View style={styles.goalsContainer}>
-        <Text>List of goals here...</Text>
+        <FlatList
+          // Usa .key automáticamente tomada de la data que pasamos
+          data={goals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            )
+          }}
+        />
       </View>
     </View>
   )
@@ -25,7 +64,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     gap: 6,
     alignItems: "center",
-    marginBottom: 24,
+    paddingBottom: 12,
+    marginBottom: 12,
     borderBottomWidth: 1,
     borderBottomColor: "gray",
   },
@@ -35,8 +75,18 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderRadius: 12,
     padding: 6,
+    paddingHorizontal: 12,
   },
   goalsContainer: {
-    flex: 6,
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    color: "white",
   },
 })
